70d298bda7471922881e75caf1fd85622976754e
ChaseCollinsSC <ccollins@switchcommerce.com>
2021-09-08 14:50:27 -0500

    hotfix for rngservice

--- src/services/rngService.js
+++ src/services/rngService.js
@@ -5,13 +5,13 @@ module.exports = {
         let rangeMaxInt = parseInt(rangeMaxStr);
 
         // rangeMaxInt must be an Integer and 0 < rangeMaxInt <= 9007199254740991 (Number.MAX_SAFE_INTEGER)
-        if(!Number.isInteger(rollNum) || rangeMaxInt <= 0 || rangeMaxInt > Number.MAX_SAFE_INTEGER){
-            message.channel.send(`Error: ${roll} is not a valid number`);
+        if(!Number.isInteger(rangeMaxInt) || rangeMaxInt <= 0 || rangeMaxInt > Number.MAX_SAFE_INTEGER){
+            message.channel.send(`Error: ${rangeMaxStr} is not a valid number`);
             return;
         }
 
         // randomize function
-        let random = Math.floor((Math.random() * rollNum) + 1);
+        let random = Math.floor((Math.random() * rangeMaxInt) + 1);
         
         // Send random integer
         message.channel.send(random);
