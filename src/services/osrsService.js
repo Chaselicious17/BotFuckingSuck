9225404c7a24df16849322f2f0aeea7dc50c864e
Chase Collins <chase.collins@live.com>
2022-06-27 22:37:16 -0500

    rewrite osrs stuff, other misc changes

--- src/services/osrsService.js
+++ src/services/osrsService.js
@@ -1,78 +1,73 @@
-const axios = require('axios');
+const hs = require('osrs-json-hiscores');
+const { hiscores } = require('osrs-gim-hiscores');
 const { logInfo } = require('../utility/logger.js');
 
 module.exports = {
-    sendStats: function (message) {
-        // Get parameter passed by message
-        let username = message.content.split(' ', 2)[1];
+    sendStats: function (message){
+        let messageTokens = message.content.split(' ');
+        let rsn = messageTokens[1];
+        let accountType = messageTokens[2];
+        if (!rsn) return;
 
-        // if no parameter passed, return
-        if (!username) return;
-
-        axios.get(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username.toLowerCase()}`)
-            .then(function (response) {
-                let highscoreData = mapHighscoreData(response.data);
-                console.log(highscoreData);
-
-                let reply = '>>> ';
-                reply += `Showing stats for: **${username}**\n`;
-                reply += `<:Attack_icon:884978564875817030> ${highscoreData.Attack.level}         <:Hitpoints_icon:884978564905177148> ${highscoreData.Hitpoints.level}         <:Mining_icon:884978564888412160> ${highscoreData.Mining.level}\n`;                
-                reply += `<:Strength_icon:884978564603211817> ${highscoreData.Strength.level}         <:Agility_icon:884978564615782491> ${highscoreData.Agility.level}         <:Smithing_icon:884978564884230164> ${highscoreData.Smithing.level}\n`;
-                reply += `<:Defence_icon:884978564745793558> ${highscoreData.Defence.level}         <:Herblore_icon:884978564737413121> ${highscoreData.Herblore.level}         <:Fishing_icon:884978564909383721> ${highscoreData.Fishing.level}\n`;
-                reply += `<:Ranged_icon:884978564884205648> ${highscoreData.Ranged.level}         <:Thieving_icon:884978564607397970> ${highscoreData.Thieving.level}         <:Cooking_icon:884978564880035850> ${highscoreData.Cooking.level}\n`;
-                reply += `<:Prayer_icon:884978564930342962> ${highscoreData.Prayer.level}         <:Crafting_icon:884978564942938142> ${highscoreData.Crafting.level}        <:Firemaking_icon:884978564951318559> ${highscoreData.Firemaking.level}\n`;
-                reply += `<:Magic_icon:884978564573831209> ${highscoreData.Magic.level}         <:Fletching_icon:884978564594819113> ${highscoreData.Fletching.level}         <:Woodcutting_icon:884978565001658408> ${highscoreData.Woodcutting.level}\n`;
-                reply += `<:Runecraft_icon:884978564892614716> ${highscoreData.Runecrafting.level}         <:Slayer_icon:884978564909371392> ${highscoreData.Slayer.level}         <:Farming_icon:884978565010059335> ${highscoreData.Farming.level}\n`;
-                reply += `<:Construction_icon:884978564968108072> ${highscoreData.Construction.level}         <:Hunter_icon:884978564993265684> ${highscoreData.Hunter.level}       Total: ${highscoreData.Overall.level}\n`;
-                reply += '';            
-
-                message.channel.send(reply);
-            })
-            .catch(function (error){
-                // handle error
-                message.channel.send(`User: ${username} not found`);
-            });
+        if (!accountType){
+            hs.getStats(rsn)
+                .then((res) => message.channel.send(buildMainResponse(res)))
+                .catch((err) => message.channel.send(`${err}`));  
+        } else if (accountType === 'group' || accountType === 'groupiron' || accountType === 'gim') {
+            hiscores.getUserStats(rsn)
+                .then((res) => message.channel.send(buildGimResponse(rsn, res)))
+                .catch((err) => message.channel.send(`${err}`));
+        } else {
+            hs.getStatsByGamemode(rsn, accountType)
+                .then((res) => message.channel.send(buildIronResponse(rsn, accountType, res.skills)))
+                .catch((err) => message.channel.send(`${err}`)); 
+        }
     }
+
 };
 
-function mapHighscoreData(hsData){
-    hsData = hsData.split('\n');
-
-    let categoryStats = new Array(hsData.length);
-    for (i = 0; i < hsData.length - 1; i++) {
-        let rankLvlExp = hsData[i].split(',');
-
-        categoryStats[categories[i]] = {
-            rank: rankLvlExp[0],
-            level: padLeft(rankLvlExp[1]),
-            experience: rankLvlExp[2]
-        };
-    }
-
-    return categoryStats;
+function buildMainResponse(r){
+    return formatResponse(r.name, r.mode, r.main.skills);
 }
 
-function padLeft(num){
-    if (num <= 9 && num >= 0){
-        return '0' + num;
-    }
-    return num;
+function buildIronResponse(name, type, skills){
+    return formatResponse(name, type, skills);
 }
 
-const categories =
-    ['Overall', 'Attack', 'Defence', 'Strength', 'Hitpoints', 'Ranged', 'Prayer', 'Magic',
-        'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing',
-        'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecrafting', 'Hunter',
-        'Construction', 'League Points', 'Bounty Hunter - Hunter', 'Bounty Hunter - Rogue',
-        'Clue Scrolls (all)', 'Clue Scrolls (beginner)', 'Clue Scrolls (easy)', 'Clue Scrolls (medium)',
-        'Clue Scrolls (hard)', 'Clue Scrolls (elite)', 'Clue Scrolls (master)', 'LMS - Rank',
-        'Soul Wars Zeal', 'Abyssal Sire', 'Alchemical Hydra', 'Barrows Chests', 'Bryophyta',
-        'Callisto', 'Cerberus', 'Chambers of Xeric', 'Chambers of Xeric: Challenge Mode',
-        'Chaos Elemental', 'Chaos Fanatic', 'Commander Zilyana', 'Corporeal Beast',
-        'Crazy Archaeologist', 'Dagannoth Prime', 'Dagannoth Rex', 'Dagannoth Supreme',
-        'Deranged Archaeologist', 'General Graardor', 'Giant Mole', 'Grotesque Guardians',
-        'Hespori', 'Kalphite Queen', 'King Black Dragon', 'Kraken', 'Kree\'Arra',
-        'K\'ril Tsutsaroth', 'Mimic', 'Nightmare', 'Obor', 'Placeholder', 'Sarachnis', 'Scorpia', 'Skotizo',
-        'Tempoross', 'The Gauntlet', 'The Corrupted Gauntlet', 'Theatre of Blood',
-        'Theatre of Blood: Hard Mode', 'Thermonuclear Smoke Devil', 'TzKal-Zuk',
-        'TzTok-Jad', 'Venenatis', 'Vet\'ion', 'Vorkath', 'Wintertodt', 'Zalcano', 'Zulrah'];
+function buildGimResponse(name, skills){
+    return formatGimResponse(name, skills);
+}
+
+function formatResponse(name, mode, skills){
+    let reply = '>>> ';
+        reply += `Showing stats for: **${name}**\n`;
+        reply += `Account Type: **${mode}**\n`;
+        reply += `<:Attack_icon:884978564875817030>  ${skills.attack.level}         <:Hitpoints_icon:884978564905177148>  ${skills.hitpoints.level}         <:Mining_icon:884978564888412160>  ${skills.mining.level}\n\n`;                
+        reply += `<:Strength_icon:884978564603211817>  ${skills.strength.level}         <:Agility_icon:884978564615782491>  ${skills.agility.level}         <:Smithing_icon:884978564884230164>  ${skills.smithing.level}\n\n`;
+        reply += `<:Defence_icon:884978564745793558>  ${skills.defence.level}         <:Herblore_icon:884978564737413121>  ${skills.herblore.level}         <:Fishing_icon:884978564909383721>  ${skills.fishing.level}\n\n`;
+        reply += `<:Ranged_icon:884978564884205648>  ${skills.ranged.level}         <:Thieving_icon:884978564607397970>  ${skills.thieving.level}         <:Cooking_icon:884978564880035850>  ${skills.cooking.level}\n\n`;
+        reply += `<:Prayer_icon:884978564930342962>  ${skills.prayer.level}         <:Crafting_icon:884978564942938142>  ${skills.crafting.level}         <:Firemaking_icon:884978564951318559>  ${skills.firemaking.level}\n\n`;
+        reply += `<:Magic_icon:884978564573831209>  ${skills.magic.level}         <:Fletching_icon:884978564594819113>  ${skills.fletching.level}         <:Woodcutting_icon:884978565001658408>  ${skills.woodcutting.level}\n\n`;
+        reply += `<:Runecraft_icon:884978564892614716>  ${skills.runecraft.level}          <:Slayer_icon:884978564909371392>  ${skills.slayer.level}         <:Farming_icon:884978565010059335>  ${skills.farming.level}\n\n`;
+        reply += `<:Construction_icon:884978564968108072>  ${skills.construction.level}         <:Hunter_icon:884978564993265684>  ${skills.hunter.level}       Total:  ${skills.overall.level}\n`;
+        reply += '';
+
+    return reply;
+}
+
+function formatGimResponse(name, skills){
+    let reply = '>>> ';
+        reply += `Showing stats for: **${name}**\n`;
+        reply += `Account Type: **Group Ironman**\n`;
+        reply += `<:Attack_icon:884978564875817030>  ${skills.attack.level}         <:Hitpoints_icon:884978564905177148>  ${skills.hitpoints.level}         <:Mining_icon:884978564888412160>  ${skills.mining.level}\n\n`;                
+        reply += `<:Strength_icon:884978564603211817>  ${skills.strength.level}         <:Agility_icon:884978564615782491>  ${skills.agility.level}         <:Smithing_icon:884978564884230164>  ${skills.smithing.level}\n\n`;
+        reply += `<:Defence_icon:884978564745793558>  ${skills.defence.level}         <:Herblore_icon:884978564737413121>  ${skills.herblore.level}         <:Fishing_icon:884978564909383721>  ${skills.fishing.level}\n\n`;
+        reply += `<:Ranged_icon:884978564884205648>  ${skills.ranged.level}         <:Thieving_icon:884978564607397970>  ${skills.thieving.level}         <:Cooking_icon:884978564880035850>  ${skills.cooking.level}\n\n`;
+        reply += `<:Prayer_icon:884978564930342962>  ${skills.prayer.level}         <:Crafting_icon:884978564942938142>  ${skills.crafting.level}         <:Firemaking_icon:884978564951318559>  ${skills.firemaking.level}\n\n`;
+        reply += `<:Magic_icon:884978564573831209>  ${skills.magic.level}         <:Fletching_icon:884978564594819113>  ${skills.fletching.level}         <:Woodcutting_icon:884978565001658408>  ${skills.woodcutting.level}\n\n`;
+        reply += `<:Runecraft_icon:884978564892614716>  ${skills.runecrafting.level}          <:Slayer_icon:884978564909371392>  ${skills.slayer.level}         <:Farming_icon:884978565010059335>  ${skills.farming.level}\n\n`;
+        reply += `<:Construction_icon:884978564968108072>  ${skills.construction.level}         <:Hunter_icon:884978564993265684>  ${skills.hunter.level}       Total:  ${skills.overall.level}\n`;
+        reply += '';
+
+    return reply;
+}
